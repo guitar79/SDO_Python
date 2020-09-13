4f75bd3e531314b394c20a66373310ba3127b950
Kiehyun Kevin Park <guitar79@naver.com>
2020-09-13 22:20:20 +0900

    Add files via upload

--- make_SDO_image_categorylist_from_filelist.py
+++ make_SDO_image_categorylist_from_filelist.py
@@ -21,20 +21,22 @@ if not os.path.exists(save_dir_name):
     print ('='*80)
     print (save_dir_name, 'is created')
 
-               
-SDO_filelists = sorted(glob(os.path.join('../SDO_filelists/SDO_filelist_*.txt')))
-
-all_list = []
-for SDO_filelist in SDO_filelists:   
-    read_filename = SDO_filelist
-    read_file = open(read_filename,'r')
-    raw_lists = read_file.read()
-    url_lists = raw_lists.split('\n')
-    suffix_list = [s for s in url_lists if suffix in s]
-    print(url_lists) #debug
-    all_list.extend(suffix_list)
-    
-with open("{0}{1}_lists.txt".format(save_dir_name, suffix), "w") as text_file:
-    text_file.write("\n".join(all_list))
-    print ("{0}{1}_lists.txt is created...".format(save_dir_name, suffix))
+for yr in range(2010,2020) :
+    SDO_filelists = sorted(glob(os.path.join('../SDO_filelists/SDO_filelist_{}*.txt'.format(str(yr)))))
+    try : 
+        all_list = []
+        for SDO_filelist in SDO_filelists:   
+            read_filename = SDO_filelist
+            read_file = open(read_filename,'r')
+            raw_lists = read_file.read()
+            url_lists = raw_lists.split('\n')
+            suffix_list = [s for s in url_lists if suffix in s]
+            print(url_lists) #debug
+            all_list.extend(suffix_list)
+            
+        with open("{0}{1}_lists{2}.txt".format(save_dir_name, suffix, str(yr)), "w") as text_file:
+            text_file.write("\n".join(all_list))
+            print ("{0}{1}_lists{2}.txt is created...".format(save_dir_name, suffix, str(yr)))
+    except Exception as err : 
+        print(err)
             
