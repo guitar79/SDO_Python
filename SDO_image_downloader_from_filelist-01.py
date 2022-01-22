5403088c07746fa7cb08676ad2427eb1f43552b8
Kiehyun Kevin Park <guitar79@gs.hs.kr>
2022-01-22 13:21:57 +0900

    update

--- SDO_image_downloader_from_filelist-01.py
+++ SDO_image_downloader_from_filelist-01.py
@@ -9,17 +9,30 @@
 
 from glob import glob
 from datetime import datetime
+import pandas as pd
 import os
-import urllib.request
 import SDO_utilities
+
+log_dir = "logs/"
+log_file = "{}{}.log".format(log_dir, os.path.basename(__file__)[:-3])
+err_log_file = "{}{}_err.log".format(log_dir, os.path.basename(__file__)[:-3])
+print ("log_file: {}".format(log_file))
+print ("err_log_file: {}".format(err_log_file))
 
 # some variables for downloading (site, file, perid and time gap, etc.)
 site = 'https://sdo.gsfc.nasa.gov/assets/img/browse/'
-targets = ['2048_HMII.jpg','1024_HMII.jpg', '1024_0304.jpg', '1024_4500.jpg', '1024_HMIIB.jpg', '1024_HMIIC.jpg', '1024_0171.jpg', '1024_0131.jpg'] #this tpye of image will be downloading
 
+#this tpye of image will be added
+img_sizes = [4096, 3072, 2048, 1024, 512]
+chls = ['0131', '0171', '0193', '0211',
+        '0304', '0094', '0335', '1600', '1700', '0211', '4500', 
+        'HMIB', 'HMIBC', 'HMID', 'HMII', 'HMIIC', 'HMIIF']
+        #'211193171', '211193171n', '211193171rg']
 
-                        
-filelist_dir_name = '../SDO_lists/'
+targets = []
+for img_size in img_sizes :
+    for chl in chls : 
+        targets.append('{0}_{1}'.format(str(img_size), chl))
 
 SDO_filelists = sorted(glob(os.path.join('{0}SDO_filelist_*.txt'.format(filelist_dir_name))))
 
